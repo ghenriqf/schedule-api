05aa1c48434544168ca4bea53d4eaed4b99247a8
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-18 21:46:08 -0300

    feat: implementar método de validação de token no TokenService

--- src/main/java/com/ghenriqf/schedule/config/security/TokenService.java
+++ src/main/java/com/ghenriqf/schedule/config/security/TokenService.java
@@ -2,11 +2,14 @@ package com.ghenriqf.schedule.config.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.ghenriqf.schedule.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -24,5 +27,24 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("ScheduleApp")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT
+                    .require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(decode.getClaim("id").asLong())
+                    .email(decode.getSubject())
+                    .build());
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
     }
 }
