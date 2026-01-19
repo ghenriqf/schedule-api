9d0a4a5710ade3e1960846fab30940f7183569ac
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-18 21:48:01 -0300

    feat: adiciona controller de autenticação com endpoints de login e cadastro

--- src/main/java/com/ghenriqf/schedule/controller/AuthenticationController.java
+++ src/main/java/com/ghenriqf/schedule/controller/AuthenticationController.java
@@ -1,19 +1,51 @@
 package com.ghenriqf.schedule.controller;
 
-import com.ghenriqf.schedule.dto.request.SignUpRequest;
-import com.ghenriqf.schedule.dto.response.SignUpResponse;
+import com.ghenriqf.schedule.config.security.TokenService;
+import com.ghenriqf.schedule.dto.request.LoginRequest;
+import com.ghenriqf.schedule.dto.request.UserRequest;
+import com.ghenriqf.schedule.dto.response.LoginResponse;
+import com.ghenriqf.schedule.dto.response.UserResponse;
+import com.ghenriqf.schedule.entity.User;
+import com.ghenriqf.schedule.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthenticationController {
 
-    // @PostMapping("/login")
-   // public LoginResponse login (@Valid @RequestBody LoginRequest request) {}
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    // @PostMapping("/signup")
-    // public SignUpResponse signUp (@Valid @RequestBody SignUpRequest request) {}
+    @PostMapping("/login")
+   public ResponseEntity<LoginResponse> login (@Valid @RequestBody LoginRequest request) {
+        try {
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+
+            Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+            User user = (User) authentication.getPrincipal();
+            String token = tokenService.generateToken(user);
+
+            return ResponseEntity.ok(new LoginResponse(token));
+        } catch (BadCredentialsException exception) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponse> signUp (@Valid @RequestBody UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
 
 }
