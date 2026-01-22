1f67b86da452c77e0f5d67c1f482cd36da754177
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-22 20:50:55 -0300

    feat: adiciona serviço de ministério com criação automática de administrador

---
+++ src/main/java/com/ghenriqf/schedule/ministry/controller/MinistryController.java
@@ -0,0 +1,11 @@
+package com.ghenriqf.schedule.ministry.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ministry")
+public class MinistryController {
+
+
+}
---
+++ src/main/java/com/ghenriqf/schedule/ministry/service/MinistryService.java
@@ -0,0 +1,37 @@
+package com.ghenriqf.schedule.ministry.service;
+
+import com.ghenriqf.schedule.auth.context.CurrentUserProvider;
+import com.ghenriqf.schedule.auth.entity.User;
+import com.ghenriqf.schedule.member.service.MemberService;
+import com.ghenriqf.schedule.ministry.dto.request.MinistryRequest;
+import com.ghenriqf.schedule.ministry.dto.response.MinistryResponse;
+import com.ghenriqf.schedule.ministry.entity.Ministry;
+import com.ghenriqf.schedule.ministry.mapper.MinistryMapper;
+import com.ghenriqf.schedule.ministry.repository.MinistryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MinistryService {
+
+    private final MinistryRepository ministryRepository;
+    private final MemberService memberService;
+    private final CurrentUserProvider currentUserProvider;
+
+    public MinistryResponse save (MinistryRequest request) {
+
+        Ministry ministry = Ministry
+                .builder()
+                .name(request.name())
+                .description(request.description())
+                .build();
+
+        Ministry save = ministryRepository.save(ministry);
+
+        User currentUser = currentUserProvider.getCurrentUser();
+        memberService.createAdmin(currentUser, ministry);
+
+        return MinistryMapper.toResponse(save);
+    }
+}
