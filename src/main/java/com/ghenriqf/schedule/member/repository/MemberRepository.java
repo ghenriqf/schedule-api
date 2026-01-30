c6e4a63562b78977251298f29023766d33d7c193
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-30 16:13:07 -0300

    feat: adiciona método de busca de membro por usuário e ministério no repositório

--- src/main/java/com/ghenriqf/schedule/member/repository/MemberRepository.java
+++ src/main/java/com/ghenriqf/schedule/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.ghenriqf.schedule.member.repository;
 import com.ghenriqf.schedule.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Long countByMinistryId(Long id);
+    Optional<Member> findByUserIdAndMinistryId(Long userId, Long ministryId);
 }
