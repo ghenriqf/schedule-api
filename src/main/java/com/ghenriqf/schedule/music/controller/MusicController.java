b750ab46990e4303f968f3eec8ad860355366f85
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-02 18:06:13 -0300

    feat: adicionar endpoints para listar e deletar músicas por ministério

--- src/main/java/com/ghenriqf/schedule/music/controller/MusicController.java
+++ src/main/java/com/ghenriqf/schedule/music/controller/MusicController.java
@@ -1,5 +1,6 @@
 package com.ghenriqf.schedule.music.controller;
 
+import com.ghenriqf.schedule.common.dto.PageRequestDTO;
 import com.ghenriqf.schedule.music.dto.request.MusicRequest;
 import com.ghenriqf.schedule.music.dto.response.MusicResponse;
 import com.ghenriqf.schedule.music.service.MusicService;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +21,16 @@ public class MusicController {
     @PostMapping("/ministries/{ministryId}/musics")
     public ResponseEntity<MusicResponse> create (@Valid @RequestBody MusicRequest musicRequest, @PathVariable Long ministryId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(musicService.save(ministryId, musicRequest));
+    }
+
+    @DeleteMapping("/ministries/{ministryId}/musics")
+    public ResponseEntity<MusicResponse> delete (@PathVariable Long musicId, @PathVariable Long ministryId) {
+        musicService.delete(musicId, ministryId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/ministries/{ministryId}/musics")
+    public ResponseEntity<List<MusicResponse>> listAll (@Valid PageRequestDTO pageRequestDTO, @PathVariable Long id ) {
+        return ResponseEntity.ok(musicService.findAll(pageRequestDTO, id));
     }
 }
