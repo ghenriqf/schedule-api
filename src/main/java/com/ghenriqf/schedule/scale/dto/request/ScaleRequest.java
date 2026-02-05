907f956063680f4dd3996572614c71512bc40d46
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-05 18:54:18 -0300

    feat: adicionar dtos de requisição e resposta para escala

---
+++ src/main/java/com/ghenriqf/schedule/scale/dto/request/ScaleRequest.java
@@ -0,0 +1,15 @@
+package com.ghenriqf.schedule.scale.dto.request;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ScaleRequest(
+        @NotEmpty(message = "Name is required") String name,
+        String description,
+        @Future LocalDateTime date
+) {
+}
---
+++ src/main/java/com/ghenriqf/schedule/scale/dto/response/ScaleResponse.java
@@ -0,0 +1,21 @@
+package com.ghenriqf.schedule.scale.dto.response;
+
+import com.ghenriqf.schedule.member.dto.response.MemberResponse;
+import com.ghenriqf.schedule.music.dto.response.MusicResponse;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Builder
+public record ScaleResponse(
+        Long id,
+        Long ministryId,
+        String name,
+        String description,
+        LocalDateTime date,
+        Set<MemberResponse> participants,
+        List<MusicResponse> musics
+) {
+}
