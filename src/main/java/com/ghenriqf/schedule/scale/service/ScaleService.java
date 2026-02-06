0a7d9e23407809208d82174a81c68c662885b669
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-06 17:17:00 -0300

    feat: implementar criação de escala com validação de administrador e vínculo ao ministério

--- src/main/java/com/ghenriqf/schedule/scale/service/ScaleService.java
+++ src/main/java/com/ghenriqf/schedule/scale/service/ScaleService.java
@@ -1,5 +1,17 @@
 package com.ghenriqf.schedule.scale.service;
 
+import com.ghenriqf.schedule.auth.context.CurrentUserProvider;
+import com.ghenriqf.schedule.auth.entity.User;
+import com.ghenriqf.schedule.common.exception.AccessDeniedException;
+import com.ghenriqf.schedule.common.exception.ResourceNotFoundException;
+import com.ghenriqf.schedule.member.dto.response.MemberResponse;
+import com.ghenriqf.schedule.member.service.MemberService;
+import com.ghenriqf.schedule.ministry.entity.MinistryRole;
+import com.ghenriqf.schedule.ministry.repository.MinistryRepository;
+import com.ghenriqf.schedule.scale.dto.request.ScaleRequest;
+import com.ghenriqf.schedule.scale.dto.response.ScaleResponse;
+import com.ghenriqf.schedule.scale.entity.Scale;
+import com.ghenriqf.schedule.scale.mapper.ScaleMapper;
 import com.ghenriqf.schedule.scale.repository.ScaleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +23,27 @@ import java.time.LocalDateTime;
 public class ScaleService {
 
     private final ScaleRepository scaleRepository;
+    private final CurrentUserProvider currentUserProvider;
+    private final MemberService memberService;
+    private final MinistryRepository ministryRepository;
 
+    public ScaleResponse create (ScaleRequest scaleRequest, Long ministryId) {
+        User currentUser = currentUserProvider.getCurrentUser();
+        MemberResponse member = memberService.findByUserIdAndMinistryId(currentUser.getId(), ministryId);
+
+        if (!(member.role().equals(MinistryRole.ADMIN))) {
+            throw new AccessDeniedException("Only administrators can create a scale");
+        }
+
+        Scale scale = ScaleMapper.toEntity(scaleRequest);
+
+        scale.setMinistry(ministryRepository.findById(ministryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ministry not found with id: " + ministryId)));
+
+        Scale save = scaleRepository.save(scale);
+
+        return ScaleMapper.toResponse(save);
+    }
 
     public Long countByMinistryIdAndDateAfter (Long id, LocalDateTime date) {
         return scaleRepository.countByMinistryIdAndDateAfter(id, date);
