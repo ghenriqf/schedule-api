de77791d831b3d90b1c6ac1f14b1e3d7413c2ed8
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-06 22:40:48 -0300

    feat: adicionar endpoints para adicionar e remover músicas de escalas

--- src/main/java/com/ghenriqf/schedule/scale/controller/ScaleController.java
+++ src/main/java/com/ghenriqf/schedule/scale/controller/ScaleController.java
@@ -1,15 +1,13 @@
 package com.ghenriqf.schedule.scale.controller;
 
+import com.ghenriqf.schedule.music.dto.response.MusicResponse;
 import com.ghenriqf.schedule.scale.dto.request.ScaleRequest;
 import com.ghenriqf.schedule.scale.dto.response.ScaleResponse;
 import com.ghenriqf.schedule.scale.service.ScaleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +18,15 @@ public class ScaleController {
     @PostMapping("/ministries/{ministryId}/scales")
     public ResponseEntity<ScaleResponse> create (@RequestBody ScaleRequest scaleRequest, @PathVariable Long ministryId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scaleService.create(scaleRequest, ministryId));
+    }
+
+    @PostMapping("/scales/{scaleId}/musics/{musicId}")
+    public ResponseEntity<MusicResponse> addMusic (@PathVariable Long scaleId, @PathVariable Long musicId) {
+        return ResponseEntity.ok().body(scaleService.addMusic(musicId, scaleId));
+    }
+
+    @DeleteMapping("/scales/{scaleId}/musics/{musicId}")
+    public ResponseEntity<MusicResponse> removeMusic (@PathVariable Long scaleId, @PathVariable Long musicId) {
+        return ResponseEntity.ok().body(scaleService.removeMusic(musicId, scaleId));
     }
 }
