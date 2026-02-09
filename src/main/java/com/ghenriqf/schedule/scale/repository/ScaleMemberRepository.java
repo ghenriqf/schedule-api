3f5c3bf32882dca5094f593ef4a365b80899bb5b
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-09 18:27:25 -0300

    feat(scale): adicionar repositório para gerenciar membros da escala

---
+++ src/main/java/com/ghenriqf/schedule/scale/repository/ScaleMemberRepository.java
@@ -0,0 +1,7 @@
+package com.ghenriqf.schedule.scale.repository;
+
+import com.ghenriqf.schedule.scale.entity.ScaleMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScaleMemberRepository extends JpaRepository<ScaleMember, Long> {
+}
