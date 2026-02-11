38393f30adf14fdb419545def9cbb03ddc0dd887
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-11 16:40:24 -0300

    fix: adicionar mensagem de validação para data futura na criação de escala

--- src/main/java/com/ghenriqf/schedule/scale/dto/request/ScaleRequest.java
+++ src/main/java/com/ghenriqf/schedule/scale/dto/request/ScaleRequest.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public record ScaleRequest(
         @NotEmpty(message = "Name is required") String name,
         String description,
-        @Future
+        @Future(message = "Invalid date, the date must be in the future")
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime date,
         Long ministerId
