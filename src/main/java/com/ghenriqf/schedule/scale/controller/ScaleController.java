904988de37342e07ef5aea8eb157cbaf1af49c02
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-11 16:42:05 -0300

    feat: adicionar endpoints para listar e buscar escalas por ministério

--- src/main/java/com/ghenriqf/schedule/scale/controller/ScaleController.java
+++ src/main/java/com/ghenriqf/schedule/scale/controller/ScaleController.java
@@ -12,6 +12,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ScaleController {
@@ -23,6 +26,20 @@ public class ScaleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(scaleService.create(scaleRequest, ministryId));
     }
 
+    @GetMapping("/ministries/{ministryId}/scales")
+    public ResponseEntity<List<ScaleSummaryResponse>> listAllByAfterDate (
+            @PathVariable Long ministryId,
+            @RequestParam(required = false) LocalDateTime dateTime
+    ) {
+        return ResponseEntity.ok().body(scaleService.listAllByAfterDate(ministryId, dateTime));
+    }
+
+    @GetMapping("/ministries/{ministryId}/scales/{scaleId}")
+    public ResponseEntity<ScaleResponse> findById (@PathVariable Long ministryId, @PathVariable Long scaleId) {
+        return ResponseEntity.ok(scaleService.findById(ministryId, scaleId));
+    }
+
+
     @PostMapping("/scales/{scaleId}/musics/{musicId}")
     public ResponseEntity<MusicResponse> addMusic (@PathVariable Long scaleId, @PathVariable Long musicId) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scaleService.addMusic(musicId, scaleId));
@@ -30,7 +47,7 @@ public class ScaleController {
 
     @DeleteMapping("/scales/{scaleId}/musics/{musicId}")
     public ResponseEntity<MusicResponse> removeMusic (@PathVariable Long scaleId, @PathVariable Long musicId) {
-        return ResponseEntity.ok().body(scaleService.removeMusic(musicId, scaleId));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(scaleService.removeMusic(scaleId, musicId));
     }
 
     @PostMapping("/scales/{scaleId}/members/{memberId}")
