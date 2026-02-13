22dd8851977ba3035cf1433b571153ae8278593e
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-02-13 14:08:05 -0300

    feat: adicionar método de validação de membro na escala e busca

--- src/main/java/com/ghenriqf/schedule/scale/repository/ScaleMemberRepository.java
+++ src/main/java/com/ghenriqf/schedule/scale/repository/ScaleMemberRepository.java
@@ -3,5 +3,9 @@ package com.ghenriqf.schedule.scale.repository;
 import com.ghenriqf.schedule.scale.entity.ScaleMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ScaleMemberRepository extends JpaRepository<ScaleMember, Long> {
+    boolean existsByScaleIdAndMemberId(Long scaleId, Long memberId);
+    Optional<ScaleMember> findByScaleIdAndMemberId(Long scaleId, Long memberId);
 }
