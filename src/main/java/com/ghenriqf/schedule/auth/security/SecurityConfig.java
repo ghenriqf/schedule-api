a1ef0b6ecd8f6e7ccac2d2ebb6c470ca1373a3b6
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-03-18 15:50:08 -0300

    feat: adicionar permição para requisições do tipo PATCH a api

--- src/main/java/com/ghenriqf/schedule/auth/security/SecurityConfig.java
+++ src/main/java/com/ghenriqf/schedule/auth/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
 
