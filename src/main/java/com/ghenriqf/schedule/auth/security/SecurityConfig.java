259cd0b464bdbe9d7d543b9056e870a22fde7124
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-03-20 15:49:34 -0300

    fix: corrige configuração de CORS para permitir frontend em produção

--- src/main/java/com/ghenriqf/schedule/auth/security/SecurityConfig.java
+++ src/main/java/com/ghenriqf/schedule/auth/security/SecurityConfig.java
@@ -49,7 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://schedule-client-app.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
